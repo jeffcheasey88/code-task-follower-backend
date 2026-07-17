06e915c59c5f0e3fdd332bb22fc59e2826b14a07
jeffcheasey88 <jeffcheasey88@peerat.dev>
2026-07-17 18:18:05 +0200

    Task permissions changeset

--- src/be/jeffcheasey88/codetaskfollower/repository/changeset/PermissionChangeSet.java
+++ src/be/jeffcheasey88/codetaskfollower/repository/changeset/PermissionChangeSet.java
@@ -2,6 +2,7 @@ package be.jeffcheasey88.codetaskfollower.repository.changeset;
 
 import java.util.List;
 
+import be.jeffcheasey88.codetaskfollower.model.Task;
 import be.jeffcheasey88.codetaskfollower.model.Player;
 import be.jeffcheasey88.codetaskfollower.model.Project;
 import be.jeffcheasey88.codetaskfollower.repository.ChangeSetApplier;
@@ -34,6 +35,7 @@ public class PermissionChangeSet implements LogicalChangeSet{
 	public void applyAfter(){
 		List<Player> players = TreasureCache.<Player>selectAll().toList();
 		List<Project> projects = TreasureCache.<Project>selectAll().toList();
+		List<Task> tasks = TreasureCache.<Task>selectAll().toList();
 		for(Player player : players){
 			for(Project project : projects){
 				TemporalRepository.INSTANCE.externalUpdateRequest(
@@ -41,6 +43,15 @@ public class PermissionChangeSet implements LogicalChangeSet{
 						new SqlParam("String","player"),
 						new SqlParam("int",player.getId()),
 						new SqlParam("int",project.getId()),
+						new SqlParam("int", Permission.PERM_ADMIN)
+					);
+			}
+			for(Task task : tasks){
+				TemporalRepository.INSTANCE.externalUpdateRequest(
+						"INSERT INTO TaskAccess(roleType, roleId, taskId, accessLevel) VALUES (?,?,?,?)",
+						new SqlParam("String","player"),
+						new SqlParam("int",player.getId()),
+						new SqlParam("int",task.getId()),
 						new SqlParam("int", Permission.PERM_ADMIN)
 					);
 			}
